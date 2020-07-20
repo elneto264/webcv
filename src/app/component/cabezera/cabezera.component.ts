270121d8afeed00601cd57d72e6af8ca39dcf445
Ernesto <ernestoster@gmail.com>
2020-07-20 20:38:51 +0200

    barra de status web

--- src/app/component/cabezera/cabezera.component.ts
+++ src/app/component/cabezera/cabezera.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-cabezera',
   templateUrl: './cabezera.component.html',
   styleUrls: ['./cabezera.component.css']
 })
+
 
 export class CabezeraComponent implements OnInit {
 
